583e32ac1aee25569d13d158cbbf060bd37c3001
6notes <jlu6.proton@proton.me>
2025-04-10 22:03:59 -0700

    Update linting for packages

--- packages/functions/eslint.config.js
+++ packages/functions/eslint.config.js
@@ -1,16 +1,8 @@
-import eslint from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
-import prettier from 'eslint-config-prettier';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import sortKeysCustomOrder from 'eslint-plugin-sort-keys-custom-order';
+import sixNotesRules from 'eslint-config-6notes-typescript';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
-  extends: [
-    eslint.configs.all,
-    ...tseslint.configs.strictTypeChecked,
-    prettier,
-  ],
+  extends: [...sixNotesRules],
   files: ['**/*.{ts,tsx}'],
   ignores: ['**/sst-env.d.ts'],
   languageOptions: {
@@ -18,24 +10,5 @@ export default tseslint.config({
       projectService: true,
       tsconfigRootDir: import.meta.dirname,
     },
-  },
-  plugins: {
-    '@stylistic': stylistic,
-    'simple-import-sort': simpleImportSort,
-    'sort-keys-custom-order': sortKeysCustomOrder,
-  },
-  rules: {
-    '@stylistic/spaced-comment': 'warn',
-    'capitalized-comments': 'off',
-    'func-style': 'off',
-    'init-declarations': 'off',
-    'one-var': ['error', 'never'],
-    'no-ternary': 'off',
-    'prefer-const': 'error',
-    'require-await': 'error',
-    'simple-import-sort/exports': 'error',
-    'simple-import-sort/imports': 'error',
-    'sort-imports': 'off',
-    'sort-keys': 'off',
   },
 });
